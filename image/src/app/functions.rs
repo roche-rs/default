71f05b7a164381c95a23a08ece97ede1f4d5ec9d
Anthony Whalley <anton@venshare.com>
2020-12-16 03:07:43 +0000

    update sample to include request and surf docs

--- image/src/app/functions.rs
+++ image/src/app/functions.rs
@@ -1,5 +1,15 @@
-pub fn handler() -> tide::Server<()> {    
+pub fn handler() -> tide::Server<()> {
     let mut api = tide::new();
-    api.at("/").get(|_| async { Ok("Hello, world!") });
+    api.at("/").get(|req: tide::Request<()>| async move {
+        // See request attributes 
+        // https://docs.rs/tide/0.15.0/tide/struct.Request.html
+        println!("Request Method : {}", req.method());
+
+        // See surf documentation 
+        // https://docs.rs/surf/2.1.0/surf/
+        let uri = "https://httpbin.org/get";
+        let string: String = surf::get(uri).recv_string().await?;
+        Ok(string)
+    });
     api
 }
--- src/functions.rs
+++ src/functions.rs
@@ -1,6 +1,12 @@
 pub fn handler() -> tide::Server<()> {
     let mut api = tide::new();
-    api.at("/").get(|_| async {
+    api.at("/").get(|req: tide::Request<()>| async move {
+        // See request attributes 
+        // https://docs.rs/tide/0.15.0/tide/struct.Request.html
+        println!("Request Method : {}", req.method());
+
+        // See surf documentation 
+        // https://docs.rs/surf/2.1.0/surf/
         let uri = "https://httpbin.org/get";
         let string: String = surf::get(uri).recv_string().await?;
         Ok(string)
